0cdace87cbedeadf4f0dcbb91576b7638b5c2a2d
Shayan Golestanipour <g.shayan5529@gmail.com>
2024-02-04 09:13:51 +0330

    the route fixe

--- src/App/AppRoute.jsx
+++ src/App/AppRoute.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./MainLayout";
 import Dashboard from "../components/Dashboard";
+import Login from "../components/auth/Login";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </MainLayout>
     </Fragment>
